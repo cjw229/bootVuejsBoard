4263cf23576a392fce00bbc79d14272522451582
나윤호 <lucky7_nyh@naver.com>
2017-11-12 17:11:46 +0900

    feat: 착한가게 화면에서 불러오기

--- board/src/main/java/com/nyh/board/article/controller/ArticleController.java
+++ board/src/main/java/com/nyh/board/article/controller/ArticleController.java
@@ -1,17 +1,28 @@
 package com.nyh.board.article.controller;
 
 import com.nyh.board.article.dto.ArticleDto;
+import com.nyh.board.article.model.Article;
+import com.nyh.board.article.service.ArticleService;
 import com.sun.deploy.net.HttpResponse;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ArticleController {
 
-    @PostMapping("/api/Article")
-    public void writeArticl(@RequestBody ArticleDto dto, HttpResponse response){
+    @Autowired
+    ArticleService articleService;
 
+    @PostMapping("/api/article")
+    public Long writeArticl(@RequestBody ArticleDto dto, HttpResponse response){
+        Long articleId =  articleService.wrtieArticle(dto).getId();
 
+        return articleId;
     }
+
+    @GetMapping("/api/article/{articleId}")
+    public Article readArticle(@PathVariable Long articleId){
+        return articleService.selectArticle(articleId);
+    }
+
 }
